eea610d726883bbb49e4cd8dce831a91212a55ba
Hoomanias <hooman.sarvi@gmail.com>
2021-03-15 21:49:39 -0400

    Chips Pages
    Chips brand page

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+
+if(document.getElementById('chips_brands_lays').checked) {
+    window.alert(chips_brands_lays + "You Selected Lays");
+  }else if(document.getElementById("chips_brands_ruffles").checked) {
+
+    window.alert(chips_brands_lays + "You Selected Ruffles");
+  }
